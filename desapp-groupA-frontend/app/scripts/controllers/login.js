3ac1608dfe24c274897e78c7710f7a1cea5b5e94
Leandro Gomez <leandro.gz73@gmail.com>
2016-04-07 01:29:29 -0300

    Add basi login. refs:#4

---
+++ desapp-groupA-frontend/app/scripts/controllers/login.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name desappGroupABackendApp.controller:AboLoginCtrlutCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the desappGroupABackendApp
+ */
+angular.module('desappGroupABackendApp')
+    .controller('LoginCtrl', function() {
+
+    });
