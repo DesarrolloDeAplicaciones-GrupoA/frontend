ffdfacd64015315a5e703956846a6b04ce5a207b
Leandro Gomez <leandro.gz73@gmail.com>
2016-04-06 22:11:48 -0300

    First version of generated app. refs:#1

---
+++ desapp-groupA-frontend/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name desappGroupABackendApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the desappGroupABackendApp
+ */
+angular.module('desappGroupABackendApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ desapp-groupA-frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name desappGroupABackendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the desappGroupABackendApp
+ */
+angular.module('desappGroupABackendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
