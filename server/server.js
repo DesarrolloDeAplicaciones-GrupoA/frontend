c65d03c0224edb33cb5cc50166d12f95f905d4a2
Leandro Gomez <leandro.gz73@gmail.com>
2016-04-14 01:21:22 -0300

    Se agrega express server para integracion con heroku

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path = require('path')
+const express = require('express')
+
+module.exports = {
+  app: function () {
+    const app = express()
+    const indexPath = path.join(__dirname, '../dist/index.html')
+    app.use(express.static(path.join(__dirname, '../dist')));
+
+    app.get('/', function (_, res) { res.sendFile(indexPath) })
+
+    return app
+  }
+}
