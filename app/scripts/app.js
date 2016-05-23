d3579f31032aea848c76d5e715cfa896b94b19ae
Leandro Gomez <leandro.gz73@gmail.com>
2016-05-23 01:44:21 -0300

    Se implementa el login con local storage y google sigin

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,44 +9,50 @@
  * Main module of the application.
  */
 angular
-    .module('desappGroupABackendApp', [
-        'ngAnimate',
-        'ngCookies',
-        'ngResource',
-        'ngRoute',
-        'ngSanitize',
-        'ngTouch',
-        'uiGmapgoogle-maps'
-    ])
-    .config(function($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'views/main.html',
-                controller: 'MainCtrl',
-                controllerAs: 'main'
-            })
-            .when('/login', {
-                templateUrl: 'views/login.html',
-                controller: 'LoginCtrl',
-                controllerAs: 'login'
-            })
-            .when('/routes', {
-                templateUrl: 'views/routes.html',
-                controller: 'RoutesCtrl',
-                controllerAs: 'routes'
-            })
-            .when('/about', {
-                templateUrl: 'views/about.html',
-                controller: 'AboutCtrl',
-                controllerAs: 'about'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    }).config(function(uiGmapGoogleMapApiProvider) {
-        uiGmapGoogleMapApiProvider.configure({
-            key: '', // Ask for this!
-            v: '3.23', //defaults to latest 3.X anyhow
-            libraries: 'weather,geometry,visualization'
-        });
+  .module('desappGroupABackendApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'uiGmapgoogle-maps',
+    'LocalStorageModule'
+  ])
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when("/products", {
+        templateUrl: 'views/products.html',
+        controller: 'ProductsCtrl',
+        controllerAs: 'products'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl',
+        controllerAs: 'login'
+      })
+      .when('/routes', {
+        templateUrl: 'views/routes.html',
+        controller: 'RoutesCtrl',
+        controllerAs: 'routes'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }).config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+      key: '', // Ask for this!
+      v: '3.23', //defaults to latest 3.X anyhow
+      libraries: 'weather,geometry,visualization'
     });
+  });
--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,6 +8,22 @@
  * Controller of the desappGroupABackendApp
  */
 angular.module('desappGroupABackendApp')
-    .controller('LoginCtrl', function() {
-
-    });
+  .controller('LoginCtrl', ["$scope", '$window', "LoginService", "localStorageService", function(
+    $scope,
+    $window,
+    loginService,
+    localStorageService) {
+    const googleSigin = document.querySelector('google-signin');
+    googleSigin.addEventListener("google-signin-offline-success", function(
+      auth) {
+      loginService.googleLogin(auth.detail.code)
+        .then(function(response) {
+            debugger;
+            localStorageService.set("acccessToken", response.data.token);
+            $window.location.assign("/");
+          },
+          function(error) {
+            console.log(error);
+          })
+    })
+  }]);
