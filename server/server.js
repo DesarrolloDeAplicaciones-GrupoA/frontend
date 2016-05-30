e0c407a24f85880c89b3293d47669bb885af69bc
Leandro Gomez <leandro.gz73@gmail.com>
2016-05-29 22:11:07 -0300

    Se agregan archivos staticos requeridos para servirlos en staging

--- server/server.js
+++ server/server.js
@@ -2,12 +2,15 @@ const path = require('path')
 const express = require('express')
 
 module.exports = {
-  app: function () {
+  app: function() {
     const app = express()
     const indexPath = path.join(__dirname, '../dist/index.html')
     app.use(express.static(path.join(__dirname, '../dist')));
+    app.use(express.static(path.join(__dirname, '../bower_components')));
 
-    app.get('/', function (_, res) { res.sendFile(indexPath) })
+    app.get('/', function(_, res) {
+      res.sendFile(indexPath)
+    })
 
     return app
   }
