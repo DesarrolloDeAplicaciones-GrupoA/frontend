ebe07db0b697e31495656e1b6c93f90e53289a33
damianlattenero <damianlattenero@gmail.com>
2016-06-28 21:45:01 -0300

    Agregada frecuencia en travels

--- app/scripts/controllers/travels/new.js
+++ app/scripts/controllers/travels/new.js
@@ -8,25 +8,49 @@
  * Controller of the desappGroupABackendApp
  */
 angular.module('desappGroupABackendApp')
-  .controller('NewTravelCtrl', function($scope, TravelService, $window) {
+  .controller('NewTravelCtrl', function ($scope, TravelService, $window) {
     $scope.newTravel = {
       nameTravel: "",
       fuel: 0,
       toll: 0,
       route: "",
-      inicio: 0,
-      fin: 0,
+      inicio: "",
+      fin: "",
+      frequency: []
     };
-    $scope.save = function(travel) {
+    $scope.days = [
+        {id: 1, text: 'lunes'},
+        {id: 2, text: 'martes'},
+        {id: 3, text: 'miercoles'},
+        {id: 4, text: 'jueves'},
+        {id: 5, text: 'viernes'},
+        {id: 6, text: 'sabado'},
+        {id: 7, text: 'domingo'}
+    ];
+
+    $scope.save = function (travel) {
 
       travel.inicio = Date.parse(travel.inicio);
       travel.fin = Date.parse(travel.fin);
 
-      TravelService.save(travel).then(function(response) {
+      TravelService.save(travel).then(function (response) {
           $window.location.assign('/#/travels/' + response.data.id);
         },
-        function(error) {
+        function (error) {
           console.log(error);
         });
     };
+    $scope.checkAll = function () {
+      $scope.newTravel.frequency = $scope.days.map(function (item) {
+        return item.id;
+      });
+    };
+    $scope.uncheckAll = function () {
+      $scope.newTravel.frequency = [];
+    };
+    $scope.checkFirst = function () {
+      $scope.newTravel.frequency.splice(0, $scope.newTravel.frequency.length);
+      $scope.newTravel.frequency.push(1);
+    };
   });
+
