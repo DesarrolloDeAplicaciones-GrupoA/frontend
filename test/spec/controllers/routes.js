4f60521c7fa6b2f4a3a13f48423f7f610b75a187
Leandro Gomez <leandro.gz73@gmail.com>
2016-07-09 15:09:44 -0300

    Fix tests

--- test/spec/controllers/routes.js
+++ test/spec/controllers/routes.js
@@ -1,31 +1,31 @@
 'use strict';
 
-describe('Controller: RoutesCtrl', function () {
+describe('Controller: TravelsCtrl', function() {
 
   // load the controller's module
   beforeEach(module('desappGroupABackendApp'));
 
-  var RoutesCtrl,
+  var TravelsCtrl,
     scope;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function($controller, $rootScope) {
     scope = $rootScope.$new();
-    RoutesCtrl = $controller('RoutesCtrl', {
+    TravelsCtrl = $controller('TravelsCtrl', {
       $scope: scope
-      // place here mocked dependencies
+        // place here mocked dependencies
     });
   }));
 
-  it('should attach a map', function () {
+  it('should attach a map', function() {
     expect(scope.map).not.toBeUndefined();
   });
 
-  it('should attach a map with a center', function () {
+  it('should attach a map with a center', function() {
     expect(scope.map.center).not.toBeUndefined();
   });
 
-  it('should attach a map with a zoom', function () {
+  it('should attach a map with a zoom', function() {
     expect(scope.map.zoom).not.toBeUndefined();
   });
 });
